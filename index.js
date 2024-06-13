f280a97849c05434643b8e8cddb3bf508908d6dc
ashidhamuneer123 <147942430+ashidhamuneer123@users.noreply.github.com>
2024-06-13 13:39:43 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//creating a promise such that only after clicking a button it is resolved or rejected
+
+/*const myPromise = new Promise((res,rej)=>{
+    document.getElementById("mybtn").addEventListener("click",()=>{
+        res("Resolved")
+    })
+    document.getElementById("mybtn2").addEventListener("click",()=>{
+        rej("Rejected")
+    })
+    
+}).then(res=>console.log(res)).catch(rej=>console.log(rej))*/
+
+
+//create a promise with out using new Promise 
+
+//async function myFun(){
+    //return "Ashidha"    //resolving a promise
+    //throw new Error       //rejecting a promise
+
+    
+//}
+//const res = myFun()
+//console.log(res);
+
+document.getElementById("header").innerHTML="ashiii"
+
+
+
+
+
